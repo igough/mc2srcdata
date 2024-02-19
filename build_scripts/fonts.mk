f3e189b07149be6250b4209e62db32467a257723
igough <igough57@gmail.com>
2024-02-19 12:03:20 -0500

    Only use a single font (Mecha) and resize to fit nicely into the UI

--- build_scripts/fonts.mk
+++ build_scripts/fonts.mk
@@ -11,46 +11,46 @@ print_fonts_target:
 	echo $(FONTS_GLYPH_FILES)
 
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial8.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arial8
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial8.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 13 $(FONTS_OUTPUT)/arial8
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial9game.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arial9game
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial9game.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/arial9game
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial9chat.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arial9chat
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arial9chat.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/arial9chat
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow8.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arialnarrow8
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow8.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 13 $(FONTS_OUTPUT)/arialnarrow8
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow9.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arialnarrow9
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow9.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/arialnarrow9
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialblack9.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/arialblack9
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialblack9.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/arialblack9
 
 
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb8.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/agencyfb8
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb8.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 13 $(FONTS_OUTPUT)/agencyfb8
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb11.bmp: $(SRC_DATA_ROOT)/fonts/emulogic.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/emulogic.ttf 8 $(FONTS_OUTPUT)/agencyfb11
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb11.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 16 $(FONTS_OUTPUT)/agencyfb11
 
 
 
 $(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb14.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/agencyfb14
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 19 $(FONTS_OUTPUT)/agencyfb14
 
 $(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb17.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/agencyfb17
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 22 $(FONTS_OUTPUT)/agencyfb17
 
 $(FONTS_OUTPUT)$(PATH_SEPARATOR)agencyfb20.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 14 $(FONTS_OUTPUT)/agencyfb20
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 25 $(FONTS_OUTPUT)/agencyfb20
 
 
-$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow6.bmp: $(SRC_DATA_ROOT)/fonts/6px2bus.ttf
-	./text_tool $(SRC_DATA_ROOT)/fonts/6px2bus.ttf 6 $(FONTS_OUTPUT)/arialnarrow6
+$(FONTS_OUTPUT)$(PATH_SEPARATOR)arialnarrow6.bmp: $(SRC_DATA_ROOT)/fonts/Mecha.ttf
+	./text_tool $(SRC_DATA_ROOT)/fonts/Mecha.ttf 11 $(FONTS_OUTPUT)/arialnarrow6
 
 
 $(FONTS_OUTPUT): $(DST_ASSETS_ROOT)
